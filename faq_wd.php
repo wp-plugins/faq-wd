43e6a45b7f077a14d496be5463b800bebee85fd9
webdorado <webdorado@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-10-12 16:08:00 +0000

    New release
    git-svn-id: https://plugins.svn.wordpress.org/faq-wd/tags/1.0.8@1264179 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ faq_wd.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Plugin Name: FAQ WD
+ * Plugin URI: https://web-dorado.com/products/wordpress-faq-wd.html 
+ * Description: Do you need an elegant FAQ section to describe details of your services, terms and conditions? You have a long company history and want to have it in Q&A format? Then FAQ WD will be the most convenient tool for reaching a highly professional result. 
+ * Version: 1.0.8
+ * Author: WebDorado
+ * Author URI: https://web-dorado.com/
+ * License: GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+
+
+if (!defined('FAQ_DIR') && !defined('FAQ_URL') && !defined('FAQ_BASENAME')) {
+    define('FAQ_DIR', dirname(__FILE__));
+    define('FAQ_URL', plugin_dir_url(__FILE__));
+    define('FAQ_BASENAME', plugin_basename(__FILE__));
+    require_once('faq_class.php');
+
+    add_action('plugins_loaded', array('faq_class', 'get_instance'));
+    if (is_admin()) {
+        require_once('admin_class.php');
+        $faq_admin = faq_admin_class::get_instance();
+    }
+}
